1b92125a8c57dc0e89d496b44713303ddb7374b6
Charles Julian Knight <charles@rabidaudio.com>
2014-03-02 02:01:43 -0500

    pre-skeleton

--- contact.js
+++ contact.js
@@ -8,7 +8,7 @@ function send_email(){
 }
 
 $.fn.extend({
-    onEnter: function(callback){
+    onKeyEnter: function(callback){
         var cb = (typeof callback == 'function') ? callback : new Function(callback);
         this.keypress(function(e){
             if( e.keyCode == 13 ){ setTimeout(cb,0,e); }
@@ -16,26 +16,52 @@ $.fn.extend({
     }
 });
 
-$(document).ready(function(){
-
-    $('#frm_submit').click(function(){
-        $.overlay.create(
-            '<div id="cp">'+
-                '<span id="img_captcha"></span><br>'+
-                '<input type="button" id="refresh_captcha" value="refresh"></input><br>'+
-                '<input type="text" id="txt_captcha"></input>'+
-            '</div>'
-        );
+function secure_contact(){
+    $.overlay.create(
+        '<div>'+
+            '<p>Please inter the following text in the block below for security purposes.<p>'+
+            '<span id="img_captcha"></span>'+
+            '<input type="button" id="refresh_captcha" value="refresh"></input><br>'+
+            '<input type="text" id="txt_captcha"></input>'+
+        '</div>'
+    );
+    $('#img_captcha').append( Captcha.generate() );
+    $("#refresh_captcha").click(function(){
         $('#img_captcha').append( Captcha.generate() );
-        $("#refresh_captcha").click(function(){
-            $('#img_captcha').append( Captcha.generate() );
-        });
-        $("#txt_captcha").onEnter(function(){
-            if(Captcha.check( $('#txt_captcha').val() )){
-                alert("yay");
-            }else{
-                alert("boo");
-            }
-        });
     });
+    $("#txt_captcha").onKeyEnter(function(){
+        if(Captcha.check( $('#txt_captcha').val() )){
+            alert("yay");
+        }else{
+            alert("boo");
+        }
+    });
+}
+
+function(){
+
+}
+
+$(document).ready(function(){
+    $('#btn_contact_submit') //TODO this aint right
+        .mouseover(function(){
+            $(this).off('mouseover');
+            $(this).animate({
+                 opacity: 1,
+                 left: "+=10"
+            },250, function(){
+                $(this).on('mouseover');
+            });
+        })
+         .mouseout(function(){
+            $(this).off('mouseout');
+            $(this).animate({
+                opacity: 0.25,
+                left: "-=10"
+            },150, function(){
+                $(this).on('mouseout');
+            });
+        })
+        .click(secure_contact);
+    //$('#txt_contact_message').onKeyEnter(secure_contact);
 });
--- jquery.overlay.js
+++ jquery.overlay.js
@@ -7,28 +7,9 @@ jQuery.overlay = (function($){
     if($===undefined) throw "jQuery is required to use this module";
     var module={};
     module.on = false;
-    $(document.head).append(
-        '<style>                                \
-        .overlay{                               \
-            z-index: 100;                       \
-            position: absolute;                 \
-            top: 0px;                           \
-            height: 100%;                       \
-            left: 0px;                          \
-            width: 100%;                        \
-            background-color: rgb(0, 0, 0);     \
-            opacity: 0.45;                      \
-            display: block;                     \
-        }                                       \
-        .overbox{                               \
-            z-index: 200;                       \
-            position: absolute;                 \
-            background-color: white;            \
-            border-radius: 5px;                 \
-            text-align: center;                 \
-        }                                       \
-        </style>'
-    );
+    /*$(document.head).append(
+        '<link rel="stylesheet" type="text/css" href="jquery.overlay.css">'
+    );*/
     module.update = function(){
         var ob = $('#ol-overbox')[0];
         //Vertical
@@ -43,7 +24,7 @@ jQuery.overlay = (function($){
         
     module.create = function(content){
         module.destroy();
-        $(document.body).append( '<div id="ol-overbox" class="overbox"></div>' );
+        $(document.body).append( '<div id="ol-overbox" class="overbox white-bg"></div>' );
         $(document.body).append( '<div id="ol-overlay" class="overlay"></div>' );
         var ob = $('#ol-overbox')[0];
         $(ob).append(content);
