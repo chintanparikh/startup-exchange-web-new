550902ff41ba4ad6ada04b56202af84c564d7531
Chintan Parikh <chintan@pennywhale.com>
2014-07-07 13:56:33 -0400

    Added responsive stylesheet and better hovers

---
+++ js/hovers.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	$(".hoverswitch").each(function(index, element) {
+		$(element).hover(function() {
+			switchTo = $(element).data('switch')
+			$(element).data('switch', $(element).attr('src'));
+			$(element).attr('src', switchTo);
+		});
+	})
+});
